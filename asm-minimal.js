fbdabbdcd3d285e215a2ded48afb56eb863b8efd
John Morris Beck <john.morris.beck@Gmail.com>
2017-07-06 23:38:39 -0700

    Update asm-minimal.js

--- asm-minimal.js
+++ asm-minimal.js
@@ -1,62 +1,80 @@
 function(c, a){ // b: [bytecode]]
 
+    //can be used to get the properties of a string    
+    //Object.getOwnPropertyNames(String.prototype)
 
-    //bytecode is memory
+    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects
+    //fundimental object
+
+    
+    //load [] from db
+    //load {} from db
+    //load true from db
+    //load false from db
+    //undefined can be retrieved by accessing the index of an array
+    //that doesn't exist
+    //NaN can be retrieved by substracting a number from a string
+
+    //in order to load a javascript object from bytecode onto the stack
+    //get the memory at the endex of that object and jump
+
+    //
+    
+
+    //m is memory and is populated with bytecode
     var m=a.b
+
+    //m[0] is the instruction in memory to run next.
+    //     It must be start higher than this list counts to.
+    //m[1] is the state to decide if the machine is running.
+    //     If it is set to true then the machine will stop and return.
+    //m[2] Is the return value of the machine.
+    //m[3] is the store. It is a reliable place to store things like pointers.
+    //it starts out as nothing but you should make it an empty object
+    //m[4] is String which can be used to convert between strings and integers
+    m=[5,1,,,String].concat(m)
     
-    //loc is the instruction in memory to run
-    //    m=[m.length,[],{}].concat(m)
-    //load [] from lib
-    //load {} from lib
-    //load true from lib
-    //load false from lib
-    m=[3,1,0].concat(m)
-    
-    //stack is how you cache data on the machine and how computatoin is done
+    //stack is how you cache data on the machine and how computation is done
     s=[]
 
+    //push onto the stack
     var p=x=>s.push(x)
 
+    //take from the stack
     var t=()=>s.pop()
-    
+
+    //turn a pointer into a copy of the pointer's value
+    var c=x=>x
+
+
+    //The loop continues until m[1] is false
+    //Then the machine returns m[2]
     while(m[1]){
-	
+
+	//the loop passes over a switch statement making a finite state machine. The switch is based on the m[0]
 	switch(m[0]){
 	    
-	case 0:
-	    
-	    //set memory
-	    
-	    m[t()]=t()
-
-	    break
 
 	case 1:
+	    
+	    //Run whatever function is on the stack with the arguments of the list behind it in the stack
+	    //javascript list behind it
 
-	    //get memory
-
-	    p(m[t()])
+	    t().apply(null, t())
 
 	    break
 
 	case 2:
 	    
-	    //case to run a function that is on the top of the stack with arguments of the
-	    //javascript list behind it
+	    //get the index of an array or the key of an object
 
-	    t().apply(null, cache(t()))
-
-	    break
-
-	case 3:
-	    
-	    //get the index of an array or the key of an object at pop
+	    //can be used to get "string"["charCodeAt"]()
 
 	    p(t[t()])
 
 	    break
 
-	case 4:
+	case 3:
 	    
 	    //subtraction
 
@@ -64,7 +82,7 @@ function(c, a){ // b: [bytecode]]
 
 	    break
 
-	case 5:
+	case 4:
 
 	    //exactly equals
 
@@ -72,7 +90,7 @@ function(c, a){ // b: [bytecode]]
 
 	    break
 
-	case 6:
+	case 5:
 
 	    //less than
 	    //may want to replace with <=
@@ -82,20 +100,22 @@ function(c, a){ // b: [bytecode]]
 	    break
 
 
-	case 7:
+	case 6:
 
 	    //logical if
 	    //if statements always deal with a jump
-	    //if the statement is true it will do the first jump
-	    //or else it will do the second jump
-	    //if saveop location saveop 0 jump saveop location saveop 0 jump
+	    //if the statement is true it will do the second jump
+	    //or else it will do the first jump
 
-	    p()&&(m[0]+=5)
+
+
+	    if(p()){p()
+		    p()}
 
 
 	    break
 
-	case 8:
+	case 7:
 
 	    //set the index of an array or the key/value of an object on the stack
 
@@ -107,41 +127,35 @@ function(c, a){ // b: [bytecode]]
 
 	    break
 
-	case 9:
 
-	    //turn a char into an int
+	case 8:
+	    
+	    //push a pointer to memory onto the stack
+	    
+	    //can be used to get something from memory
+	    //can be used to set something in memory
+	    //can be used to return the length of memory
 
-	    p(t(charCodeAt()))
+	    p(m)
 
 	    break
+
+	case 9:
+
+	    //push the next op onto the stack
+	    //necessary for pushing args onto the stack to begin with
+
+	    p(m[m[0]])
+
+	    m[0]++
 
 	case 10:
 
-	    //turn an int into a char
+	    //turn a reference into a copy of that reference
 
-	    p(String.fromCharCode(t()))
+	    p(c(t()))
 
-	    break
-
-	case 11:
-
-	    //return the length of memory
-
-	    p(m.length)
-
-	    break
-
-	case 12:
-
-	    p(#db.i(t()))
-
-	    break
-
-	case 13:
-
-	    p(#db.f(t()).first())
-
-	    break
+	    
 
 	}
 	
